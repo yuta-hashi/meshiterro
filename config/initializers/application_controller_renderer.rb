d289d8e730fc140d62629fde08f02a10595d1805
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-15 23:45:57 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'daf60911b4115892fc51a3d9179a9e5c6f472fb1552b59edc854726db360c02cb0eb648cea7dff952d799f67fe10194b3b560ff5367bb5fccd210002068ae715'
