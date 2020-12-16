b4730b45f45d348443292153532313ba28c0df55
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-16 23:43:38 +0900

    [Finish]5-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'post_images#index'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 end
