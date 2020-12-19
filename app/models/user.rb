7ed6283614a3b4b3bc837412d0abc32bd4482f1b
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-19 18:54:31 +0900

    [Finish]5-6

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :post_images, dependent: :destroy
-         has_many :post_comments, dependent: :destroy
-         has_many :favorites, dependent: :destroy
-         attachment :profile_image
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image
 end
